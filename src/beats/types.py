3f96c91da8d7276c59272d0310e500cbe54836b3
elvijs <elvijsdr@gmail.com>
2024-05-13 20:46:45 +0100

    A tiny app to play around with librosa visualisations

--- src/beats/types.py
+++ src/beats/types.py
@@ -7,3 +7,8 @@ import numpy.typing as npt
 
 
 Vector = NewType("Vector", npt.NDArray[np.complex64])
+SamplingRate = NewType("SamplingRate", float)
+Tempo = NewType("Tempo", float)  # in beats per minute
+
+# A 1D array where the samples represent sound amplitude at subsequent time instants
+Song = NewType("Song", npt.NDArray[np.complex64])
